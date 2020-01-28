3e3620d0a04beca239d8453a925ecea3f67d8086
4l3j4ndr0cruz1233 <hcruz0716@gmail.com>
2020-01-27 18:32:02 -0600

    Add function showMap & initMap

--- main.js
+++ main.js
@@ -1,7 +1,51 @@
-var map;
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
 function initMap() {
+  // The location of méxico
+  var méxico = {lat: 24.8186288, lng: -101.9745723};
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 5, center: méxico});
+  // The marker, positioned at méxico
+}
+
+
+var map, infoWindow;
+function showMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    zoom: 16
   });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Aquí estas.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+} 
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: Tu localizacion no se pudo obtener.' :
+    'Error: Tu navegador no soporta Geolocalización');
+  infoWindow.open(map);
 }
+
